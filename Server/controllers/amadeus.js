6f6367e1a0351d9e4646c3106b9052f068f8728a
Brett Mather <brett.mather@joby.aero>
2023-05-15 18:27:11 -0700

    adding amadeus and a route in the controllers folder

---
+++ Server/controllers/amadeus.js
@@ -0,0 +1,34 @@
+import Amadeus from "amadeus"
+
+var amadeus = new Amadeus({
+  clientId: 'bDfd8aWbh5FM5U3u6XATzYf4jG2IWT7b',
+  clientSecret: 'lSXjU1GstFcGZ0ia'
+});
+
+const location = async (req, res) => {
+  // Airports and City Search (autocomplete)
+  // Find all the cities and airports starting by 'LON'
+  amadeus.referenceData.locations.get({
+    keyword : 'LON',
+    subType : Amadeus.location.any
+  }).then(function(response){
+    console.log(response.data);
+    res.send(response.data)
+  }).catch(function(responseError){
+    console.log(responseError.code);
+  });
+
+}
+
+// amadeus.shopping.flightOffersSearch.get({
+//     originLocationCode: 'SJC',
+//     destinationLocationCode: 'SAN',
+//     departureDate: '2023-06-25',
+//     adults: '2'
+// }).then(function(response){
+//   console.log(response.data);
+// }).catch(function(responseError){
+//   console.log(responseError.code);
+// });
+
+export {location};
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { response } from "express";
+import { location } from "../controllers/amadeus.js";
 const router = express.Router();
 
 const base = async (req, res) => {
@@ -6,5 +7,7 @@ const base = async (req, res) => {
 };
 
 router.route("/").get(base);
+
+router.route("/location").get(location);
 
 export default router;
