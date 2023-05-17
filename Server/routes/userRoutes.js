9bb0adf774959d436c669ba9e0c14cbcf9e12345
mkborland <mkborland@aol.com>
2023-05-17 15:45:10 -0500

    new routes for front

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 const router = express.Router();
 
-import { addUser, requestAllUsers, requestUser } from "../controllers/userController.js";
+import { addUser, requestAllUsers, requestUser, updateUser } from "../controllers/userController.js";
 
 router.route("/add_user").post(addUser);
 
 router.route("/get_all_users").get(requestAllUsers);
 
 router.route("/get_user").get(requestUser);
+
+router.route("/update_user").post(updateUser);
 
 export default router;
