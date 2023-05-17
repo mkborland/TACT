1dbbd138ffa97c7421c23a3feb291fa1c7c8be59
d.sauerbronn <linkdayan2000@gmail.com>
2023-05-16 21:47:37 -0500

    working on the history list item.

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,6 +3,7 @@ import LoginCard from "./components/LoginCard/LoginCard.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./components/SignUp/SignUp.js";
 import PersistentDrawerLeft from "./components/MainPage/MainPage.js";
+import HistoryPage from "./pages/HistoryPage";
 
 function App() {
   return (
@@ -10,9 +11,13 @@ function App() {
       <div className="App-header">
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<PersistentDrawerLeft />}>
-              <Route path="test" element={<LoginCard />} />
-              <Route path="signup" element={<SignUp />} />
+            <Route path="/" element={<LoginCard />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/Dashboard" element={<PersistentDrawerLeft />}>
+              <Route path="NewExercisePage" element={<LoginCard />} />
+              <Route path="History" element={<HistoryPage />} />
+              <Route path="EditDataTables" element={<LoginCard />} />
+              <Route path="ManageUsers" element={<LoginCard />} />
             </Route>
           </Routes>
         </BrowserRouter>
