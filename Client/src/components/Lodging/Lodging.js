e5de21779edfc85cca67c14d4c817ebea8357cb5
mkborland <mkborland@aol.com>
2023-05-18 14:09:10 -0500

    base

---
+++ Client/src/components/Lodging/Lodging.js
@@ -0,0 +1,20 @@
+/*
+Inputs:
+    number of gov lodg
+    number of com lodg
+    number of field cond
+
+Outputs:
+    number of gov lodg
+    number of com lodg
+    number of field cond
+
+    total number of ppl
+
+    cost of gov lodg
+    cost of com lodge
+    cost of field cond = 0
+
+    total cost 
+
+*/
