66d0debaee51e3799526cd9b2f09208d790633ff
Brett Mather <brett.mather@joby.aero>
2023-05-17 17:08:47 -0700

    functional flighttable with rawdata

--- Client/src/components/FlightSearch/FlightSearch.js
+++ Client/src/components/FlightSearch/FlightSearch.js
@@ -5,16 +5,16 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import Button from '@mui/material/Button';
 import { useState, useEffect } from 'react';
 import FlightTable from "../FlightTable/FlightTable";
-
+import data from "./rawdata.json";
 
 
 const FlightSearch = () => {
-
+    
 
     return(
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <LocationField label='To:'/>
-            <LocationField label='From:'/>
+            {/* <LocationField label='To:'/> */}
+            {/* <LocationField label='From:'/> */}
             <DatePicker label="Depart" />
             <DatePicker label="Return" />
             <Button variant="contained"
@@ -24,7 +24,7 @@ const FlightSearch = () => {
             >
                 Find Flights
             </Button>
-            <FlightTable />
+            <FlightTable data={data}/>
         </LocalizationProvider>
 
 
--- Client/src/components/FlightTable/FlightTable.js
+++ Client/src/components/FlightTable/FlightTable.js
@@ -7,50 +7,59 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(
-  name,
-  calories,
-  fat,
-  carbs,
-  protein,
-) {
-  return { name, calories, fat, carbs, protein };
+class simpleflightoffer{
+    constructor(offer){
+        // console.log(offer)
+        this.departureStartAirport = offer.itineraries[0].segments[0].departure.iataCode
+        this.departureStartTime = offer.itineraries[0].segments[0].departure.at
+        this.departureEndAirport = offer.itineraries[0].segments.slice(-1)[0].arrival.iataCode
+        this.departureEndTime = offer.itineraries[0].segments.slice(-1)[0].arrival.at
+        this.departureStopovers = offer.itineraries[0].segments.length
+
+        this.returnStartAirport = offer.itineraries[1].segments[0].departure.iataCode
+        this.returnStartTime = offer.itineraries[1].segments[0].departure.at
+        this.returnEndAirport = offer.itineraries[1].segments.slice(-1)[0].arrival.iataCode
+        this.returnEndTime = offer.itineraries[1].segments.slice(-1)[0].arrival.at
+        this.returnStopovers = offer.itineraries[1].segments.length
+
+        this.cost = offer.price.total
+    }
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+const FlightTable = (props) => {
 
-export default function FlightTable() {
+    const simple_data = props.data.map(offer => {
+        // console.log(offer)
+        return new simpleflightoffer(offer)
+    })
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Flights</TableCell>
+            <TableCell align="right">Departure Trip</TableCell>
+            <TableCell align="right">Departure Stopovers</TableCell>
+            <TableCell align="right">Return Trip</TableCell>
+            <TableCell align="right">Return Stopovers</TableCell>
+            <TableCell align="right">Cost</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {simple_data.map((offer) => (
             <TableRow
-              key={row.name}
+              key={offer.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
-                {row.name}
+              <TableCell component="th" scope="offer">
+                {offer.id}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{offer.departureStartAirport}: {offer.departureStartTime} - {offer.departureEndAirport}: {offer.departureEndTime}</TableCell>
+              <TableCell align="right">{offer.departureStopovers}</TableCell>
+              <TableCell align="right">Return Trip</TableCell>
+              <TableCell align="right">{offer.returnStartAirport}: {offer.returnStartTime} - {offer.returnEndAirport}: {offer.returnEndTime}</TableCell>
+              <TableCell align="right">${offer.cost}</TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -58,3 +67,5 @@ export default function FlightTable() {
     </TableContainer>
   );
 }
+
+export default FlightTable
