3d5289aeb0762ca9e9ca84a8a094166f261036fd
mkborland <mkborland@aol.com>
2023-05-17 22:33:10 -0500

    cors sucks

--- Client/src/api/PerDiem/PerDiem.js
+++ Client/src/api/PerDiem/PerDiem.js
@@ -1,10 +1,30 @@
 const api_url = "https://api.gsa.gov/travel/perdiem/v2/rates/"
-async function getPerDiemRates() {
-    const response = await fetch(api_url + "Fairfax/state/VA/year/2019")
+const endpoint = "zip/20171/year/2022"
 
-    const data = await response.json();
-    console.log(data)
+const getPerDiemRates = async () => {
+    const response = await fetch(`${api_url}${endpoint} `, {
+        method: "GET",
+        headers: {
+            "x-api-key": "Nnp0kqZgQgjKqR9fCP94kfQl2GcJplIRifkgUNap"
+        }
+    });
 
+    if (!response.ok) {
+        console.log("error");
+    } else {
+        const data = await response.json();
+        console.log(data)
+
+        const monthlyrates = data.rates.rate.months.month.map(item => {
+            delete item.short
+            delete item.long
+        })
+        const mealrate = data.rates.rate.meals;
+        const city = data.rates.rate.city;
+
+        const perDiemRates = { monthlyrates, mealrate, city };
+        return perDiemRates;
+    }
 }
 
-getPerDiemRates();
+export default getPerDiemRates;
