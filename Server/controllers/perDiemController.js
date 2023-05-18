b93711ce37a0aa479ff0d8af06b1fb0134d4029b
mkborland <mkborland@aol.com>
2023-05-18 13:45:36 -0500

    update perdiem api to accept input

--- Server/controllers/perDiemController.js
+++ Server/controllers/perDiemController.js
@@ -1,8 +1,10 @@
-const zip = "20171"
-const year = "2022"
 
 const requestRates = async (req, res) => {
-    const response = await fetch(`https://api.gsa.gov/travel/perdiem/v2/rates/zip/${zip}/year/${year}`, {
+    const yearIn = req.query.year
+    const cityIn = req.query.city
+    const stateIn = req.query.state
+
+    const response = await fetch(`https://api.gsa.gov/travel/perdiem/v2/rates/city/${cityIn}/state/${stateIn}/year/${yearIn}`, {
         method: "GET",
         headers: {
             "x-api-key": "Nnp0kqZgQgjKqR9fCP94kfQl2GcJplIRifkgUNap"
@@ -11,16 +13,15 @@ const requestRates = async (req, res) => {
 
     if (!response.ok) {
         res.status(404).send(`There was an error fetching your data server responded with ${response}`)
-        // console.log("error");
     } else {
         const data = await response.json();
-        // console.log(data)
-        // res.status(200).send(data)
         const dumbformat = data.rates[0].rate[0];
         const monthlyrates = dumbformat.months.month;
         const mealrate = dumbformat.meals;
         const city = dumbformat.city;
-        const perDiemRates = { monthlyrates, mealrate, city };
+        const state = data.rates[0].state;
+        const year = data.rates[0].year;
+        const perDiemRates = { monthlyrates, mealrate, city, state, year };
         res.status(200).send(perDiemRates)
     }
 }
