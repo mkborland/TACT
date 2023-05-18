bd756092f349cb966ee6233b19f74ef5a3dd34e8
mkborland <mkborland@aol.com>
2023-05-18 13:18:30 -0500

    fix history page

--- Client/src/api/TactApi.js
+++ Client/src/api/TactApi.js
@@ -1,9 +1,11 @@
 import GetAllAircraft from "./Airplanes/get/GetAllAircraft";
 import GetExercises from "./exercises/get/GetExercises";
 import UpdateAircraft from "./Airplanes/update/UpdateAircraft";
+import GetAllExercises from "./exercises/get/GetAllExercises.js"
 
 const TactApi = {
-  getAExercises: GetExercises,
+  getExercises: GetExercises,
+  getAllExercises: GetAllExercises,
   getAllAircraft: GetAllAircraft,
   updateAircraft: UpdateAircraft,
 };
