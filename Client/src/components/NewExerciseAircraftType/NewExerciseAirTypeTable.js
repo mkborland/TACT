5bb9c013ae7515728058c2ecbf146cc3cac440e8
Roman Morawski <rmorawski@CONMBPFHKHX-MORAW.local>
2023-05-18 16:01:27 -0500

    input component for airTypes

--- Client/src/components/NewExerciseAircraftType/NewExerciseAirTypeTable.js
+++
@@ -1,6 +0,0 @@
-
-const AirTypeTable = () => {
-    
-}
-
-export default AirTypeTable;
--- Client/src/pages/NewExercisePage.js
+++ Client/src/pages/NewExercisePage.js
@@ -1,9 +1,11 @@
 import FlightSearch from "../components/FlightSearch/FlightSearch";
+import AirTypeTable from "../components/NewExerciseAircraftType/AirTypeTable";
 
 const NewExercisePage = () => {
   return (
     <div>
       <FlightSearch />
+      <AirTypeTable />
     </div>
   );
 };
