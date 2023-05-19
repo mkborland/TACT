1a5bb2fa2c169cd6f1d2b0a7387baa8b496c9776
Roman Morawski <rmorawski@CONMBPFHKHX-MORAW.local>
2023-05-18 21:49:53 -0500

    web page reload :(

--- Client/src/components/ManageUsersPage/UserInfoCard.js
+++ Client/src/components/ManageUsersPage/UserInfoCard.js
@@ -10,7 +10,28 @@ const UserInfo = (name, id, role) => {
 
     const handleAccept = (e,id) => {
         TactApi.updateUser({ userid : id , authkey : "54321"}).then(
-            data => console.log(data)
+            data => {
+                console.log(data);
+                window.location.reload(true);
+            }
+        )
+    }
+
+    const handlePromote = (e,id) => {
+        TactApi.updateUser({ userid : id , authkey : "12345"}).then(
+            data => {
+                console.log(data);
+                window.location.reload(true);
+            }
+        )
+    }
+
+    const handleDemote = (e,id) => {
+        TactApi.updateUser({ userid : id , authkey : "54321"}).then(
+            data => {
+                console.log(data);
+                window.location.reload(true);
+            }
         )
     }
 
@@ -18,7 +39,7 @@ const UserInfo = (name, id, role) => {
     if(role === "pending"){
     return (<Typography key={id} align="left" noWrap={true}>• UserName: {name} Role: {role} <IconButton><BlockIcon sx={{ color: red[500]}}/></IconButton><IconButton onClick={(e) => handleAccept(e,id)}><CheckCircleIcon sx={{ color: green[500]}}/></IconButton></Typography>)
     }
-    return (<Typography key={id} align="left" noWrap={true}>• UserName: {name} Role: {role}<IconButton><RemoveCircleOutlineIcon sx={{ color: red[500]}}/></IconButton><IconButton><ControlPointIcon sx={{ color: green[500]}}/></IconButton></Typography>)
+    return (<Typography key={id} align="left" noWrap={true}>• UserName: {name} Role: {role}<IconButton onClick={(e) => handleDemote(e,id)}><RemoveCircleOutlineIcon sx={{ color: red[500]}}/></IconButton><IconButton onClick={(e) => handlePromote(e,id)}><ControlPointIcon sx={{ color: green[500]}}/></IconButton></Typography>)
 }
 
 export default UserInfo
