41ce47d0972087f841d96bb3015eeba9bb72d9f2
Jason Flowers <poppyjflow@gmail.com>
2023-09-04 09:22:18 -0700

    Updated README for initial Keycloak instructions

--- Client/src/PrivateRoute.js
+++ Client/src/PrivateRoute.js
@@ -4,19 +4,6 @@ import DashboardPage from "./pages/DashboardPage";
 import { useNavigate } from 'react-router-dom';
 export default function PrivateRoute() {
 
-  // const [keycloak, setKeycloak] = React.useState("");
-  // const [authenticated, setAuthenticated] = React.useState(false);
-  // const [isLoading, setIsLoading] = useState(true);
-  // console.log("help me");
-
-
-
-  // try {
-  //     const authenticated = keycloak.init({ onLoad: 'login-required', checkLoginIframe: false});
-  //     console.log(`User is ${authenticated ? 'authenticated' : 'not authenticated'}`);
-  // } catch (error) {
-  //     console.error('Failed to initialize adapter:', error);
-  // }
   const [keycloak, setKeycloak] = React.useState("");
   const [authenticated, setAuthenticated] = React.useState(false);
   const [isLoading, setIsLoading] = useState(true);
@@ -34,8 +21,8 @@ export default function PrivateRoute() {
       "realm": "TACT",
       "url": "http://localhost:8180/",
       "clientId": "TACT-app",
-
     });
+
     const initKeycloak = async () => {
       key
         .init({
@@ -47,36 +34,18 @@ export default function PrivateRoute() {
           setAuthenticated(key.authenticated);
           alert(authenticated ? 'authenticated' : 'not authenticated');
           setIsLoading(false);
-
-
-return navigate('/Dashboard');
-
-//          return <Navigate to={{ pathname: '/Dashboard' }} />;
-
+          return navigate('/Dashboard');
         })
         .catch(function (e) {
           console.log('Failed to initialize keycloak', e);
           setIsLoading(false);
-        });
+        })
+      ;
       setIsLoading(false);
-
-      // return <Navigate to={{pathname: '/dashboard'}}/>;
-
-
     };
 
     if (isLoading) {
       initKeycloak();
     }
   }, [isLoading]);
-
-  // if (!isLoading){
-  //     console.log("Auth " + authenticated);
-  //     if(authenticated) {
-  //         console.log("keycloak");
-  //         return <Navigate to={{pathname: '/'}}/>;
-  //     } else {
-  //         return (<div>Unable to authenticate!</div>)
-  //     }
-  // }
 };
