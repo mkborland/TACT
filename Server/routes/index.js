0c46dabfbefc0e395c0f2c43ae87891d6662ad0e
Rex <73486744+shortf15e@users.noreply.github.com>
2023-09-12 09:44:20 -0700

    feat: adds roles router to server

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -3,7 +3,17 @@ import userRoutes from "./userRoutes.js";
 import exerciseRoutes from "./exerciseRoutes.js";
 import amadeusRoutes from "./amadeusRoutes.js";
 import perDiemRoutes from "./perDiemRoutes.js";
-import locationRoutes from "./locationRoutes.js"
-import analysisToolRoutes from "./analysisToolRoutes.js"
+import locationRoutes from "./locationRoutes.js";
+import analysisToolRoutes from "./analysisToolRoutes.js";
+import roleRoutes from "./roleRoutes.js";
 
-export { aircraftRoutes, userRoutes, exerciseRoutes, amadeusRoutes, perDiemRoutes, locationRoutes, analysisToolRoutes };
+export {
+  aircraftRoutes,
+  userRoutes,
+  exerciseRoutes,
+  amadeusRoutes,
+  perDiemRoutes,
+  locationRoutes,
+  analysisToolRoutes,
+  roleRoutes,
+};
